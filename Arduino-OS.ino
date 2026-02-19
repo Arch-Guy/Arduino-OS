3bf7be9847b8fe3831b7e7a8ebe4e886db305d3b
Arch-Guy <ridhansh.bumb@maneckjicooperschool.org>
2026-02-19 22:29:17 +0530

    Update Arduino-OS.ino

--- Arduino-OS.ino
+++ Arduino-OS.ino
@@ -1,37 +1,36 @@
 #include <EEPROM.h> // Include EEPROM Services
-byte buffer[1024]; // Create 1 KB RAM Buffer
 void setup() {
-	memset(buffer, 0, sizeof(buffer)); // Clear memory
-	Serial.begin(9600); // Start Serial services at 9600 baud
-	Serial.println("Welcome to Arduino OS!"); // Splash Screen
+	Serial.begin(9600);  // Start Serial Services at 9600 Baud
+	Serial.println("Welcome to Arduino BIOS!");  // Boot splash
 }
 void loop() {
-	if (Serial.available()) {
-		String input = Serial.readStringUntil('\n');
-		input.trim();
-		if (input.startsWith("write(")) {   // Write command parsing
-			int commaIndex = input.indexOf(',');
-			int quoteStart = input.indexOf('"');
-			int quoteEnd = input.lastIndexOf('"');
-			int addr = input.substring(6, commaIndex).toInt();
-			String data = input.substring(quoteStart + 1, quoteEnd);
-			int i = 0;
-			while (i < data.length()) {   // Write data
-				EEPROM.write(addr, data[i]);
-				addr++;
-				i++;
+	if (Serial.available()) {  // Input Available?
+		String input = Serial.readStringUntil('\n');  // Input
+		input.trim();  // Remove any spaces around the input
+		String* ram = new String(input); // Store input in buffer
+		if (input.startsWith("write(")) {  // Write command?
+			int commaIndex = input.indexOf(',');  // Find comma
+			int quoteStart = input.indexOf('"');  // Find double quote #1
+			int quoteEnd = input.lastIndexOf('"');  // Find double quote #2
+			int addr = input.substring(6, commaIndex).toInt();  // Fetch address
+			String data = input.substring(quoteStart + 1, quoteEnd);  // Fetch string
+			int i = 0;  // Create iterator
+			while (i < data.length()) { // Until data's length is over
+				EEPROM.update(addr, data[i]);  // Write byte at address
+				addr++;  // Increment address
+				i++;  // Increment iterator
 			}
 			Serial.println("OK");
 		}
-		else if (input.startsWith("read(")) {   // Read command parsing
-			int commaIndex = input.indexOf(',');
-			int addr = input.substring(5, commaIndex).toInt();
-			int length = input.substring(commaIndex + 1, input.length() - 1).toInt();
-			int i = 0;
-			while (i < length) {   // Read Data
-				Serial.write(EEPROM.read(addr));
-				addr++;
-				i++;
+		else if (input.startsWith("read(")) {  // Read command?
+			int commaIndex = input.indexOf(',');  // Find comma
+			int addr = input.substring(5, commaIndex).toInt();  // Find start address
+			int length = input.substring(commaIndex + 1, input.length() - 1).toInt();  // Find data length
+			int i = 0;  // Create iterator
+			while (i < length) { // Until length of data is finished
+				Serial.write(EEPROM.read(addr));  // Print out the EEPROM Data of the current address
+				addr++;  // Increment address
+				i++;  // Increment iterator
 			}
 			Serial.println();
 		}
@@ -40,3 +39,4 @@ void loop() {
 		}
 	}
 }
+/*And some notes here. If you're wondering WHY I didn't add bounds checking, it's because I LOVE that there is no extra error. I love corruptness.*/
